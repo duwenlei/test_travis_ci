37ef44071f969b91296da351f0c00f3a0cb1de1c
du_wenlei <952589031@qq.com>
2018-12-10 14:10:55 +0800

    isNotNull(null)

--- src/test/java/com/dwl/util/StringUtilsTest.java
+++ src/test/java/com/dwl/util/StringUtilsTest.java
@@ -3,12 +3,10 @@ package com.dwl.util;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class StringUtilsTest {
 
     @Test
     public void isNotNull() {
-        Assert.assertTrue(StringUtils.isNotNull(""));
+        Assert.assertTrue(StringUtils.isNotNull(null));
     }
 }
